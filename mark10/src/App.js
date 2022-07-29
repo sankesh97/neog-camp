b59d18b5cc482e074e8362f2f4f6cb2d1f696ec2
Sankesh Jain <ramojitinorange@gmail.com>
2022-07-29 12:53:37 +0530

    mark14 completed logic

--- mark10/src/App.js
+++ mark10/src/App.js
@@ -1,6 +1,22 @@
 import { useRef, useState } from "react";
 import "./App.css";
 
+const calculateFunc = (cashGiven, billAmount) => {
+  let tempChange = {};
+  let tempValue = cashGiven - billAmount;
+  for (let i of [2000, 500, 100, 50, 20, 10, 5, 2, 1]) {
+    if (tempValue > i) {
+      tempChange[i] = Math.floor(tempValue / i);
+      console.log("tempChange" + tempChange[i]);
+      tempValue -= tempChange[i] * i;
+      console.log(tempValue);
+    } else {
+      tempChange[i] = 0;
+    }
+  }
+  return tempChange;
+};
+
 function App() {
   const billAmount = useRef();
   const cashGiven = useRef();
@@ -15,8 +31,20 @@ function App() {
     2: 0,
     1: 0,
   });
+  const [outputText, setOutputText] = useState("");
 
   const returnCalculateHandler = () => {
+    if (billAmount.current.value === "" || cashGiven.current.value === "") {
+      setOutputText("Bro, Did you forget to enter the amount ?");
+    } else if (cashGiven.current.value > 50000) {
+      setOutputText(
+        "Kala Dhan Detected, Calling Modi ji! (You are not allowed to use cash more than 50,000)"
+      );
+    } else {
+      setReturnChange(
+        calculateFunc(cashGiven.current.value, billAmount.current.value)
+      );
+    }
     billAmount.current.value = "";
     cashGiven.current.value = "";
   };
@@ -24,7 +52,6 @@ function App() {
   return (
     <div className="App">
       <h1>Cash Register Manager</h1>
-      {console.log(returnChange)}
       <p>
         Enter the bill amount and cash given by the customer and know minimum
         number of notes to return.
@@ -33,41 +60,44 @@ function App() {
         <label htmlFor="billAmount">
           <strong>Bill Amount:</strong>
         </label>
-        <input ref={billAmount} id="billAmount" type="number" />
+        <input ref={billAmount} id="billAmount" type="number" min="0" />
         <br />
         <label htmlFor="CashGiven">
           <strong>Cash Given:</strong>
         </label>
-        <input ref={cashGiven} id="CashGiven" type="number" />
+        <input ref={cashGiven} id="CashGiven" type="number" min="0" />
         <br />
         <button onClick={returnCalculateHandler}>Check</button>
       </div>
+      {outputText && <p>{outputText}</p>}
       <h2>Return Change</h2>
       <table className="table">
-        <tr>
-          <td>No of Notes</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Note</td>
-          <td>₹2000</td>
-          <td>₹500</td>
-          <td>₹100</td>
-          <td>₹50</td>
-          <td>₹20</td>
-          <td>₹10</td>
-          <td>₹5</td>
-          <td>₹2</td>
-          <td>₹1</td>
-        </tr>
+        <tbody>
+          <tr>
+            <td>No of Notes</td>
+            <td>{returnChange[2000]}</td>
+            <td>{returnChange[500]}</td>
+            <td>{returnChange[100]}</td>
+            <td>{returnChange[50]}</td>
+            <td>{returnChange[20]}</td>
+            <td>{returnChange[10]}</td>
+            <td>{returnChange[5]}</td>
+            <td>{returnChange[2]}</td>
+            <td>{returnChange[1]}</td>
+          </tr>
+          <tr>
+            <td>Note</td>
+            <td>₹2000</td>
+            <td>₹500</td>
+            <td>₹100</td>
+            <td>₹50</td>
+            <td>₹20</td>
+            <td>₹10</td>
+            <td>₹5</td>
+            <td>₹2</td>
+            <td>₹1</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
