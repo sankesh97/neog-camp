622f010f4686315a2c7008539f37cf2b045d39a5
Sankesh Jain <ramojitinorange@gmail.com>
2022-07-30 23:26:00 +0530

    Fixed the error in line 8 change from > to >=

--- mark10/src/App.js
+++ mark10/src/App.js
@@ -5,7 +5,7 @@ const calculateFunc = (cashGiven, billAmount) => {
   let tempChange = {};
   let tempValue = cashGiven - billAmount;
   for (let i of [2000, 500, 100, 50, 20, 10, 5, 2, 1]) {
-    if (tempValue > i) {
+    if (tempValue >= i) {
       tempChange[i] = Math.floor(tempValue / i);
       console.log("tempChange" + tempChange[i]);
       tempValue -= tempChange[i] * i;
